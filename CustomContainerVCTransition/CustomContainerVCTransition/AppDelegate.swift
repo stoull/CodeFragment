0fc08a0fd9f286f4b7bcfb4f79302f248894dee9
stoull <chencc@linkapp.cn>
2018-01-29 20:04:42 +0800

    Remove CustomContainerVCTransition's StoryBoard

--- CustomContainerVCTransition/CustomContainerVCTransition/AppDelegate.swift
+++ CustomContainerVCTransition/CustomContainerVCTransition/AppDelegate.swift
@@ -16,7 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        
+        let rootVC = ViewController()
+        rootVC.title = "Root"
+        rootVC.view.backgroundColor =  UIColor(red: 0.4, green: 0.8, blue: 1, alpha: 1)
+        window?.rootViewController = rootVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- CustomContainerVCTransition/CustomContainerVCTransition/ViewController.swift
+++ CustomContainerVCTransition/CustomContainerVCTransition/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if label.superview == nil {
+            label.text = title
+            label.sizeToFit()
+            view.addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = true;
+            
+            let labelCenterXConstraint = NSLayoutConstraint.init(item: view, attribute: .centerX, relatedBy: .equal, toItem: label, attribute: .centerX, multiplier: 1, constant: 0)
+            let labelCenterYConstraint = NSLayoutConstraint.init(item: view, attribute: .centerY, relatedBy: .equal, toItem: label, attribute: .centerY, multiplier: 1, constant: 0)
+            
+            view.addConstraint(labelCenterXConstraint)
+            view.addConstraint(labelCenterYConstraint)
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
